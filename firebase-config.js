d4f66d789433595cca1b4a83f91ec516396c207a
williamcunhaA <africois74@gmail.com>
2025-04-18 06:36:41 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyALdL8dEBj4s7-LxP_30zfRDXLHOH3o1zE",
+  authDomain: "kwanzaplay-ae73e.firebaseapp.com",
+  databaseURL: "https://kwanzaplay-ae73e-default-rtdb.firebaseio.com",
+  projectId: "kwanzaplay-ae73e",
+  storageBucket: "kwanzaplay-ae73e.appspot.com",
+  messagingSenderId: "1022206559498",
+  appId: "1:1022206559498:web:6cf58e89b13c9c484226f3"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
